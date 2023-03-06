a8597fa65822fc022a9e29698c7e99ee431206d2
skhiev <khiev@mail.ru>
2023-03-06 23:06:01 +0400

    BtnGitHub - style.css

--- src/components/btnGitHub/BtnGitHub.js
+++ src/components/btnGitHub/BtnGitHub.js
@@ -3,10 +3,15 @@ import gitHubIcon from "./gitHub-black.svg";
 
 const BtnGitHub = ({ link }) => {
   return (
-    <a href={link} target="_blank" rel="noreferrer" className="btn-outline">
+    <button
+      href={link}
+      target="_blank"
+      rel="noreferrer"
+      className="btn-outline"
+    >
       <img src={gitHubIcon} alt="" />
       GitHub repo
-    </a>
+    </button>
   );
 };
 
